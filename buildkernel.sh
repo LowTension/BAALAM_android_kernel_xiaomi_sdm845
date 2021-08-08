bbfefa1cf2a6df2de0d503ab266ad7978d44b63c
zonarmr <zonarmr@gmail.com>
2021-08-08 11:21:15 -0500

    Update buildkernel.sh

--- buildkernel.sh
+++ buildkernel.sh
@@ -11,13 +11,13 @@ nocol='\033[0m'
 #export PATH="${HOME}/android-kernel/proton-clang/bin:${PATH}"
 
 COMPILER=clang
-#PROTON_CLANG="$HOME/android-kernel/proton-clang"
+
 # Speed up build process
+#MAKE="./makeparallel"
 
-MAKE="./makeparallel"
-
-#export KBUILD_BUILD_USER="zonarmr"
-#export KBUILD_BUILD_HOST="zonarmr"
+export USE_CCACHE=1
+export KBUILD_BUILD_USER="zonarmr"
+export KBUILD_BUILD_HOST="zonarmr"
 
 clean(){
     rm -rf out
@@ -26,17 +26,11 @@ clean(){
 }
 
 build() {
-#export ARCH=arm64
-#export SUBARCH=arm64
-#export DTC_EXT=dtc
-#${CROSS_COMPILE}ld -v
-
  make O=out ARCH=arm64 dipper_defconfig
  make -j$(nproc --all) O=out \
     ARCH=arm64 \
     CC=${COMPILER} \
     CROSS_COMPILE=aarch64-linux-gnu- \
-    LD_LIBRARY_PATH=${HOME}/android-kernel/proton-clang/lib \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- | tee kernel.log
 }
 
@@ -45,5 +39,5 @@ build
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
-echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+echo -e "$cyan Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
 
